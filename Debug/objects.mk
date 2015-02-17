96b61c548546b389da4c3ee7fb58b444c69409c8
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-02-17 13:58:02 +0100

    Remove eclipse build information from repository.

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lzmq -lboost_program_options -lboost_filesystem -lboost_system
-
